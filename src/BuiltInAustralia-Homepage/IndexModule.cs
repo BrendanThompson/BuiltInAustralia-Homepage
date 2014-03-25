ddb70dd29a2136a9eb72e61f8d4954f2b9156580
Brendan Thompson <brendan.thompson@ezidebit.com.au>
2014-03-25 12:03:23 +1000

    Initial Commit

---
+++ src/BuiltInAustralia-Homepage/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace BuiltInAustralia_Homepage
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
